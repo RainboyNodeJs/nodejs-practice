4e9b809f776e9eb87656df8f13e39323315fe897
Rainboylvx <597872644@qq.com>
2016-03-03 18:24:27 +0800

    test

---
+++ test.js
@@ -0,0 +1,23 @@
+var http = require('http');
+
+var options = {
+	host:'www.baidu.com',
+    hostname: 'www.baidu.com',  
+    port: 80,  
+    path: '/',  
+    method: 'GET'  
+};
+
+var chunk = [];
+var req = http.request(options,function(res){
+	res.on('data',function(cb){
+		console.log('BODY : '+cb)
+	});
+});
+
+req.on('error', function (e) {  
+    console.log('problem with request: ' + e.message);  
+}); 
+i=2
+++i + ++i
+req.end();
