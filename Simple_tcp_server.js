006f320ceb937fedfcba548c07bb6f57ad33190e
Rainboylvx <597872644@qq.com>
2016-03-08 18:11:02 +0800

    simple tcp server

---
+++ Simple_tcp_server.js
@@ -0,0 +1,16 @@
+var net = require('net');
+
+var server = net.createServer(function(c){
+	console.log("服务器已连接");
+	c.on("end",function(){
+		console.log("服务器已断开");
+	});
+	
+	c.write('hello\r\n');
+	c.pipe(c);
+	}
+);
+
+server.listen(8124,function(){
+	console.log("服务器已绑定");
+});
